02c8a91d1d2a1b8d6d2cd4af275cc7847bc76fae
Rahul Kumar <krahul86946@gmail.com>
2025-10-17 18:56:47 +0530

    starting a new project named Netflix-GPT

---
+++ README.md
@@ -0,0 +1,1 @@
+echo "# Netflix-GPT" >> README.md
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  
+  return (
+    <>
+      <div className="text-2xl font-bold bg-red-300 text-blue-500 ">namaste React!</div>  
+    </>
+  )
+}
+
+export default App;
