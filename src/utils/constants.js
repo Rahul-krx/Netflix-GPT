d0a759f24cca47c216e187e74894ab847b2b60ec
Rahul Kumar <krahul86946@gmail.com>
2025-10-30 21:37:55 +0530

    fetched movies list using api call from TMDB

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,11 @@
 export const LOGO = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-08-26/consent/87b6a5c0-0104-4e96-a291-092c11350111/0198e689-25fa-7d64-bb49-0f7e75f898d2/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
 export const PROFILE_BG = "https://assets.nflxext.com/ffe/siteui/vlv3/024bfb98-9e9d-441d-9c5f-d2c798d8e42f/web/IN-en-20251013-TRIFECTA-perspective_b6324826-0535-47c5-be5f-4f50e9663eaf_large.jpg";
+
+export const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZTY3YmY5ZDg1Njc5ODVmNmM1YzQ5MzQxMDZkYTk5NCIsIm5iZiI6MTc2MDI4Mzc2Mi41NTAwMDAyLCJzdWIiOiI2OGViY2M3MmZkZDYwYTU3ZjkxZjY5ZDgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0._01dUa3w0f9AHByRzfOqXSo0G9mCDN_baZ2fwKp79IM'
+  }
+};
