8b90b5fb761ad1d633c1b21cb9c108b0c29eb3d0
Rahul Kumar <krahul86946@gmail.com>
2025-11-03 23:03:17 +0530

    main container section completed

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,17 +1,26 @@
+import { useSelector } from "react-redux";
+import { API_OPTIONS } from "../utils/constants";
+import useMovieTrailer from "./hooks/useMovieTrailer";
 
-import { useSelector } from 'react-redux';
-import { API_OPTIONS } from '../utils/constants'
-import useMovieTrailer from './hooks/useMovieTrailer';
-
-
- const VideoBackground = ({movieId}) => {
+const VideoBackground = ({ movieId }) => {
   useMovieTrailer(movieId);
 
-   const trailerVideo = useSelector((store)=> store.movies?.trailerVideo);
-  
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
+  console.log("https://www.youtube.com/watch?v=" + trailerVideo?.key);
+
+
   return (
-    <div><iframe width="560" height="315" src= {"https://www.youtube.com/embed/" + trailerVideo?.key} 
-    title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin"></iframe></div>
-  )
-}
+    <div className="w-full">
+      <iframe
+        className="w-full aspect-video"
+        src={`https://www.youtube.com/embed/${trailerVideo?.key}?autoplay=1&mute=1`}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
 export default VideoBackground;
+
--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -2,23 +2,29 @@ import React from "react";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-36 px-12">
+    <div className=" w-fit aspect-video pt-[20%] px-24 absolute text-white bg-gradient-to-r from-black ">
       <h1 className="font-bold text-6xl">{title}</h1>
       <p className="text-lg w-1/4 py-6">{overview}</p>
 
       <div className="flex">
-        <button className="px-8 py-2 font-semibold bg-red-700 m-4 rounded-lg cursor-pointer flex text-xl">
+        <button className="px-4 py-2 font-semibold bg-white m-4 rounded-lg cursor-pointer flex text-xl text-black hover:opacity-80">
           {" "}
           <img
-            className="w-6 "
-            src=" public\play-button-arrowhead.png"
+            className="w-6 mx-2"
+            src="/play-button-arrowhead.png"
             alt="play icon"
-            srcset=""
+            srcSet=""
           />
           Play
         </button>
-        <button className="px-8 mt-[14px] h-11 font-semibold bg-gray-400 rounded-lg text-white cursor-pointer">
+        <button className="px-4 mt-[14px] h-11 font-semibold bg-gray-500 rounded-lg text-white cursor-pointer flex text-center py-2 text-lg opacity-85">
           {" "}
+          <img
+            className="w-6 h-6 mt-[2px] mx-1 "
+            src="/icons8-info-501.png"
+            alt=""
+            srcSet=""
+          />{" "}
           More Info
         </button>
       </div>
